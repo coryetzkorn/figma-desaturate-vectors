ff6d0092031d66c55a852ba5783971952fe28ed9
Cory Etzkorn <coryetzkorn@gmail.com>
2020-03-14 19:10:39 -0700

    First commit

---
+++ code.ts
@@ -0,0 +1,17 @@
+function clone(val) {
+  return JSON.parse(JSON.stringify(val))
+}
+
+for (const node of figma.currentPage.selection) {
+  if (node.type === "VECTOR") {
+    const fills = clone(node.fills)
+    const color = fills[0].color
+    const averageFill = (color.r + color.g + color.b) / 3
+    color.r = averageFill
+    color.g = averageFill
+    color.b = averageFill
+    node.fills = fills
+  }
+}
+
+figma.closePlugin()
